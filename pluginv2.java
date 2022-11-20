93f0cac86773d61295d9452ded9b9cdfad3f5563
Mariia <113040756+bzhemba@users.noreply.github.com>
2022-11-20 19:49:12 +0300

    Create pluginv2.java

---
+++ pluginv2.java
@@ -0,0 +1,43 @@
+import com.intellij.ide.BrowserUtil;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.ui.Messages;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+public class NotionAction extends AnAction {
+    @Override
+    public void actionPerformed(AnActionEvent event) {
+        Editor editor = event.getData(PlatformDataKeys.EDITOR);
+
+
+        String selectedText = editor.getSelectionModel().getSelectedText();
+        if (selectedText != null) {
+            String encoded = "";
+            try {
+                encoded = URLEncoder.encode(selectedText, StandardCharsets.UTF_8.toString());
+            } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException(e);
+            }
+
+            String url = String.format("https://www.notion.so/my-projects-26e8d0c6de9e4eae88ee81bdac7b1970#78c00d1b1af444269cf8515a08ce6a82", encoded);
+            Messages.showMessageDialog(url, "Notion Action", Messages.getInformationIcon());
+            BrowserUtil.browse(url);
+        }
+        else{
+            Messages.showMessageDialog("Selection is empty", "Notion Action", Messages.getInformationIcon());
+        }
+    }
+
+
+    @Override
+    public boolean isDumbAware() {
+        return super.isDumbAware();
+    }
+
+}
